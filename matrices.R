a844804f301df3060333e50c72f0c68a9dd603fb
meena sirisha145 <meenasirisha145@gmail.com>
2017-08-19 15:11:48 +0530

    d

---
+++ matrices.R
@@ -0,0 +1,32 @@
+x=matrix(1:12,nrow=4,byrow=T)
+x
+x[5]
+class(x)
+x[2,2]
+x[,c(1,2)]
+x[c(2,4),]
+x[c(1,2),3]
+dim(x)
+colnames(x)=c('sub1','sub2','sub3')
+x
+rownames(x)=paste('R',1:4,sep='')
+x
+colnames(x)=paste('c',1:3,sep='')
+x
+
+y=c(8,6,4,1)
+cbind(x,y)
+z=c(8,9,1)
+rbind(x,z)
+x
+x[,2]>6
+x[,2]>5
+x[x[,2]>5]
+
+cells = c(1,26,24,68)
+ rnames <= c("R1", "R2")
+cnames <- c("C1", "C2") 
+mymatrix <- matrix(cells, nrow=2,  byrow=F,
+                     dimnames=list(rnames, cnames))
+mymatrix
+
--- vectors.R
+++ vectors.R
@@ -36,6 +36,8 @@ x="meena"
 x
 substr(x,2,5)
 x=c(5,6,9,8,5)
+head(USArrests)
+
 x
 
 sort(x)
@@ -55,3 +57,69 @@ v=paste(1:6,8:9,sep="&")
 v
 r=paste('student',1:10,sep='-')
 r
+age <- c(1,3,5,2,11,9,3,9,12,3)
+weight <- c(4.4,5.3,7.2,5.2,8.5,7.3,6.0,10.4,10.2,6.1)
+mean(weight)
+
+sd(weight)
+cor(age,weight)
+plot(age,weight)
+v=seq(1,10,2)
+v
+rep(v,c(3))
+rep(v,v(3))
+rep(v,each=3)
+x="good morning!"
+nchar(x)
+x=c("nature's", "best")
+nchar(x)
+x=c("nature's", " at its best ")
+nchar(x)
+fname="james"
+lname="bond"
+paste(fname,lname)
+m = "Capital of America is Washington"
+substr(m,1,18)
+head(USArrests)
+states=rownames(USArrests)
+states
+substr(x=states,start=1,stop=4)
+states2=abbreviate(states)
+states2
+names(states2)=NULL
+states2
+abbreviate(states,minlength=5)
+states
+n=nchar(states)
+n
+states[which(n==max(n))]
+s="Success is not final, failed is not fatal"
+sub("failed","failure",s)
+s
+Names <- c("John", "Andrew", "Thomas") 
+Designation <- c("Manager", "Project Head", "Marketing Head")
+x=c(Names,Designation)
+x
+y=matrix(x,nrow=3,byrow=F)
+colnames(y)=c("Names","Designation")
+y
+g=paste("a":"e",c("a","b","c","d","e"))
+g
+
+x=c('a','b','c')
+y=c('a','b','c')
+paste(x,y, sep='')
+paste(rep(letters[1:5],5), c(letters[1:5],each=5))
+rep(c('a','b',))
+rep()
+x=c("a","b","c","d","e")
+paste(x,c(letters[1:5],each=5))
+
+x=c(outer(letters[1:3], letters[1:3], FUN=paste, sep=""))
+x
+LETTERS[1:30]
+month.abb[1:5]
+months.Date(5)
+month.name[1:5]
+months.Date('10/03/2017')
+julian(Sys.Date(), 1)
