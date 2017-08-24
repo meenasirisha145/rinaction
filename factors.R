78d97b9c9d4129537fd82fab6c60e2ae9eb4a682
meena sirisha145 <meenasirisha145@gmail.com>
2017-08-24 16:02:01 +0530

    j

---
+++ factors.R
@@ -0,0 +1,8 @@
+x=factor(c('Male','Female','Male'))
+x
+x=factor(c("yes","no","no"),levels=c("yes","no"))
+x
+x1=factor(c('low','medium','high'),ordered=T)
+x1
+x2=factor(c('low','medium','high'),ordered=T,levels=c("low","medium","high"))
+x2
