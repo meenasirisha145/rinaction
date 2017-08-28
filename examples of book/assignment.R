eaf49491259ac3b9b2dbb086ab90ec8c5d8c78fb
meena sirisha145 <meenasirisha145@gmail.com>
2017-08-28 18:52:10 +0530

    a

--- examples of book/assignment.R
+++ examples of book/assignment.R
@@ -16,6 +16,18 @@ m.table=table(m.cuts)
 m.table
 cbind(m.table)
 
+m2=c(12,63,61,63,87,65,1,24,67,52,70,55,29,93,30,84,29,27,19,
+24,32,31,16,4,22,82,53,92,84,5,46,27,31,51,19,76,21,26,
+100,81,56, 65,32,63,33,51,68,49,25,77)
+m2
+cat(m2)
+range(m2)
+breaks=seq(0,100,by=10)
+m2.cuts=cut(m2,breaks)
+m2.cuts
+m2.table=table(m2.cuts)
+m2.table
+cbind(m.table)
 #question:2
 
 Marks=c(10 , 12, 13, 14 , 17)
