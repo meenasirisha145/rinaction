dc0aaa9d8f7259fcd73121c65991f0b492cbd051
meena sirisha145 <meenasirisha145@gmail.com>
2017-08-29 14:07:55 +0530

    G

--- examples of book/assignment.R
+++ examples of book/assignment.R
@@ -1,8 +1,9 @@
 #question:1
 x=c(10,20,30,10,40,50)
-rep(x,c(3,5,2,2,3,4))
-x
-
+x1=rep(x,c(3,5,2,2,4,4))
+x1
+table(x1)
+mean(x1)
 #question3
 set.seed(09)
 m=ceiling(runif(50,0,100))
@@ -30,8 +31,40 @@ m2.table
 cbind(m.table)
 #question:2
 
-Marks=c(10 , 12, 13, 14 , 17)
+marks=c(10 , 12, 13, 14 , 17)
 mean(Marks)
+students=c(5,7,9,8,4)
+marks1=marks*students
+marks1
+students1=sum(students)
+mean=sum(marks1)/students1
+mean
+#other method for discrete
+x=rep(marks,students)
+table(x)
+mean(x)
+#continuous
+marks2=c("10-20","20-30","30-40","40-50")
+students2=c(5,7,9,8)
+df2=data.frame(marks2,students2)
+  str(df2)
+midpt2=c(15,25,35,45)
+
+df2$mid2=midpt2
+df2
+df2$total=df2$students2*df2$mid2
+df2$total
+df2
+sum(df2$total/sum(students2))
+
+x=rep(df2$mid2,df2$students2)
+mean(x)
+
+
+
+
+
+
 m1=ceiling(runif(29,0,50))
 set.seed(09)
 m1
