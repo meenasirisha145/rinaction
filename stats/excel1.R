498a08698cf7fbeb08019d49010d83d2f7ea47fb
meena sirisha145 <meenasirisha145@gmail.com>
2017-08-30 12:07:28 +0530

    a

---
+++ stats/excel1.R
@@ -0,0 +1,14 @@
+#moving data from excel to R
+#input data from keyboard
+x=scan()
+x
+mean(x)
+y=scan()#error for character copying
+
+?scan
+y=scan(what='character')
+y
+median(x)
+table(x)
+sort(table(x))
+sort(table(x),decreasing=TRUE)
