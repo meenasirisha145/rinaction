3c0eb388c3e63788dabdca2618209928ad54267a
meena sirisha145 <meenasirisha145@gmail.com>
2017-10-13 20:34:58 +0530

    L

--- examples of book/assignment6/asgn6.R
+++ examples of book/assignment6/asgn6.R
@@ -1,8 +1,8 @@
 #assignment6
-#creating a vector----
-set.seed(1234)
-v=ceiling(runif(40,10,200))
-v
+
+#QUESTION 1-----
+#creating a vector---
+
 s=ceiling(seq(10,200,length.out = 40))
 s
 length(s)
@@ -10,6 +10,46 @@ length(s)
 m=matrix(s,nrow=5)
 m
 #product of each row----
-m[1,]*m[2,]*m[3,]*m[4,]*m[5,]
+apply(m,1,prod)
+#sum of each column
 addmargins(m,1,sum)
+apply(m,2,sum)
+# Return a new matrix whose entries are those of 'matrix values' modulo 10
+apply(m,c(1,2),function(x) x%%10)
+#How many odd nos each column has
+y=apply(m,c(1,2),function(x) x%%2!=0)
+y
+apply(y,2,sum)
 
+
+# View the data set iris
+
+# get the mean of the first 4 variables, by species
+
+iris
+aggregate(iris[1:4],list(iris$Species),mean)
+
+
+#QUESTION 2-----
+# Environment
+# create a new environment
+e=new.env()
+e
+e$a=matrix(1:12,nrow=4)
+e$b=array(c(1:3*4*2),dim=c(3,4,2))
+e$a
+e$b
+mean(e$a)
+mean(e$b)
+class(e$a)
+class(e$b)
+
+#QUESTION 3-----
+# create a list with 2 elements 1:20 and 55:100 and mpg and weight coln of mtcars dataset
+v1=c(1:20)
+v2=c(55:100)
+l=list(v1,v2,mtcars$wt,mtcars$mpg)
+l
+# Find the sum of the values in each element
+lapply(l,sum)#print the values in the form of list
+sapply(l,sum)#print the values in the form of vector
